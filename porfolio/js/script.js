76a3380e4b2110571b1a08d2417666b2a29dce31
YeongjuJeon <yjyj620@naver.com>
2024-12-04 22:26:23 +0900

    1204 백업

---
+++ porfolio/js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
